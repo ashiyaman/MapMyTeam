f04f28e2d012cc9ad54ac8ca68580c1119c9d1d0
ashiyaman <ashiya1005@gmail.com>
2025-06-03 11:00:04 +0530

    chore"Add user schema

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'), bcrypt = require('bcrypt'), SALT_WORK_FACTOR = 10
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,6 +21,22 @@ const userSchema = new mongoose.Schema({
 },
 {
     timestamps: true
+})
+
+userSchema.pre(save, () => {
+    var user = this
+    if(!user.modified('password')) return next()
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if(err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
 })
 
 const User = mongoose.model('User', userSchema)
